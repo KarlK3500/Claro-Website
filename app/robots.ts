dde967336992cac3dff6762539c63dcc4b763391
Karl Ketelslagers <karl.ktel@gmail.com>
2026-05-27 17:12:50 +0200

    add sitemap and robots

---
+++ app/robots.ts
@@ -0,0 +1,9 @@
+export default function robots() {
+  return {
+    rules: {
+      userAgent: "*",
+      allow: "/",
+    },
+    sitemap: "https://clarobalance.com/sitemap.xml",
+  };
+}
---
+++ app/sitemap.ts
@@ -0,0 +1,8 @@
+export default function sitemap() {
+  return [
+    {
+      url: "https://clarobalance.com",
+      lastModified: new Date(),
+    },
+  ];
+}
