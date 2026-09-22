2c880252e9eeab9ba04f0662cb271e31e4345cb4
Karl Ketelslagers <karl.ktel@gmail.com>
2026-09-22 15:11:06 +0200

    Add parent family page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -22,5 +22,9 @@ export default function sitemap(): MetadataRoute.Sitemap {
       url: "https://clarobalance.com/relatiebegeleiding-hasselt",
       lastModified: new Date(),
     },
+    {
+  url: "https://clarobalance.com/ouder-en-gezinsbegeleiding-hasselt",
+  lastModified: new Date(),
+},
   ];
 }
